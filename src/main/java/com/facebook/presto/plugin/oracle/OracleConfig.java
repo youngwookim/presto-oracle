d0b158b098e0ba25e0083c82cb4c748930eb7819
Youngwoo Kim <ywkim@apache.org>
2015-11-10 18:05:00 +0900

    WIP

--- src/main/java/com/facebook/presto/plugin/oracle/OracleConfig.java
+++ src/main/java/com/facebook/presto/plugin/oracle/OracleConfig.java
@@ -19,65 +19,70 @@ import io.airlift.configuration.Config;
  * To get the custom properties to connect to the database. User, password and
  * URL is provided by de BaseJdbcClient is not required. If there is another
  * custom configuration it should be put in here.
- * 
+ *
  * @author Marcelo Paes Rech
  *
  */
-public class OracleConfig {
+public class OracleConfig
+{
+    private String user;
+    private String password;
+    private String url;
 
-	private String user;
-	private String password;
-	private String url;
+    /**
+     * @return the user
+     */
+    public String getUser()
+    {
+        return user;
+    }
 
-	/**
-	 * @return the user
-	 */
-	public String getUser() {
-		return user;
-	}
+    /**
+     * @param user
+     *            the user to set
+     */
+    @Config("oracle.user")
+    public OracleConfig setUser(String user)
+    {
+        this.user = user;
+        return this;
+    }
 
-	/**
-	 * @param user
-	 *            the user to set
-	 */
-	@Config("oracle.user")
-	public OracleConfig setUser(String user) {
-		this.user = user;
-		return this;
-	}
+    /**
+     * @return the password
+     */
+    public String getPassword()
+    {
+        return password;
+    }
 
-	/**
-	 * @return the password
-	 */
-	public String getPassword() {
-		return password;
-	}
+    /**
+     * @param password
+     *            the password to set
+     */
+    @Config("oracle.password")
+    public OracleConfig setPassword(String password)
+    {
+        this.password = password;
+        return this;
+    }
 
-	/**
-	 * @param password
-	 *            the password to set
-	 */
-	@Config("oracle.password")
-	public OracleConfig setPassword(String password) {
-		this.password = password;
-		return this;
-	}
+    /**
+     * @return the url
+     */
+    public String getUrl()
+    {
+        return url;
+    }
 
-	/**
-	 * @return the url
-	 */
-	public String getUrl() {
-		return url;
-	}
-
-	/**
-	 * @param url
-	 *            the url to set
-	 */
-	@Config("oracle.password")
-	public OracleConfig setUrl(String url) {
-		this.url = url;
-		return this;
-	}
-
+    /**
+     * @param url
+     *            the url to set
+     */
+    @Config("oracle.password")
+    public OracleConfig setUrl(String url)
+    {
+        this.url = url;
+        return this;
+    }
 }
--- src/main/java/com/facebook/presto/plugin/oracle/OracleQueryBuilder.java
+++ src/main/java/com/facebook/presto/plugin/oracle/OracleQueryBuilder.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.facebook.presto.plugin.oracle;
 
 import java.text.SimpleDateFormat;
@@ -13,23 +26,24 @@ import com.facebook.presto.spi.type.VarcharType;
 import com.google.common.primitives.Ints;
 import com.google.common.primitives.Longs;
 
-public class OracleQueryBuilder extends QueryBuilder {
-	
-	private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+public class OracleQueryBuilder extends QueryBuilder
+{
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
     private final SimpleDateFormat timestampFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	
-	public OracleQueryBuilder(String quote)
+
+    public OracleQueryBuilder(String quote)
     {
         super(quote);
     }
-	
-	@Override
-	protected String toPredicate(String columnName, String operator, Object value, Type type)
+
+    @Override
+    protected String toPredicate(String columnName, String operator, Object value, Type type)
     {
         String valueStrting;
         if (type.equals(TimestampType.TIMESTAMP)) {
             long unixtime = Longs.tryParse(value.toString());
-            valueStrting = "TO_TIMESTAMP(" + encode(timestampFormat.format(new Date(unixtime))) + ", 'yyyy-mm-dd hh24:mi:ss')";
+            valueStrting = "TO_TIMESTAMP(" + encode(timestampFormat.format(new Date(unixtime)))
+                    + ", 'yyyy-mm-dd hh24:mi:ss')";
         }
         else if (type.equals(DateType.DATE)) {
             Calendar c = Calendar.getInstance();
@@ -45,5 +59,4 @@ public class OracleQueryBuilder extends QueryBuilder {
         }
         return quote(columnName) + " " + operator + " " + valueStrting;
     }
-
 }
